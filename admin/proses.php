96d81020c9bac82018f755becd7bb5510f3cd929
mahdidham <mahdidham@Idhams-MacBook-Air.local>
2015-11-25 14:12:50 +0700

    update-database
    update database di proses

--- admin/proses.php
+++ admin/proses.php
@@ -1,11 +1,10 @@
-
-	<?php
+<?php
 	session_start();
 	
-	$user="root";
-	$pass="";
-	$host="localhost";
-	$database="transampera";
+	$user= "be403f13277093";//"root";
+	$pass="704d78d8";
+	$host= "br-cdbr-azure-south-a.cloudapp.net"; //"localhost";
+	$database= "transampera"; //"transampera";
 	
 	$koneksi=mysql_connect("$host","$user","$pass")or die(mysql_error("Internet anda tidak ada"));
 	$db=mysql_select_db($database) or die(mysql_error());
@@ -28,4 +27,4 @@
 		</script>";			
 	}
 	
-	?>
+?>
